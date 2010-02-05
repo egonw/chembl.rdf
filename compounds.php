87d64eb5d8f97d6df6c3d8e21c7483e21c09135a
Egon Willighagen <egon.willighagen@gmail.com>
2010-02-05 20:47:20 +0100

    Converted to spit N3

--- compounds.php
+++ compounds.php
@@ -1,28 +1,25 @@
-<?php header('Content-type: application/rdf+xml');
-print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"); 
-?>
-<!DOCTYPE rdf:RDF [
-  <!ENTITY gt "&#62;">
-  <!ENTITY lt "&#60;">
-  <!ENTITY ch "http://pele.farmbio.uu.se/chembl/?">
-  <!ENTITY bodo "http://www.blueobelisk.org/ontologies/chemoinformatics-algorithms/#">
-]>
-<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-         xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
-         xmlns:nmr="http://www.nmrshiftdb.org/onto#"
-         xmlns:chembl="&ch;"
-         xmlns:chem="http://www.blueobelisk.org/chemistryblogs/"
-         xmlns:dc="http://purl.org/dc/elements/1.1/"
-         xmlns:foaf="http://xmlns.com/foaf/0.1/"
-         xmlns:bodo="&bodo;"
-         xmlns:owl="http://www.w3.org/2002/07/owl#"
-         xmlns:bibo="http://purl.org/ontology/bibo/">
+<?php header('Content-type: text/n3'); ?>
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix owl: <http://www.w3.org/2002/07/owl#> .
 
-<?php 
+@prefix dc: <http://purl.org/dc/elements/1.1/> .
+@prefix bibo: <http://purl.org/ontology/bibo/> .
+@prefix foaf: <http://xmlns.com/foaf/0.1/> .
+
+@prefix bodo: <http://www.blueobelisk.org/ontologies/chemoinformatics-algorithms/#> .
+@prefix chem: <http://www.blueobelisk.org/chemistryblogs/> .
+@prefix nmr: <http://www.nmrshiftdb.org/onto#> .
+
+@prefix : <http://pele.farmbio.uu.se/chembl/onto/#> .
+@prefix act: <http://rdf.farmbio.uu.se/chembl/activitiy/> .
+@prefix res: <http://rdf.farmbio.uu.se/chembl/resource/> .
+@prefix mol: <http://rdf.farmbio.uu.se/chembl/molecule/> .
+@prefix ass: <http://rdf.farmbio.uu.se/chembl/assay/> .
+
+<?php
 
 include 'vars.php';
-
-$ns = "&ch;";
 
 #$limit = " LIMIT 5";
 $limit = "";
@@ -44,31 +41,29 @@ $descs = array(
   "med_chem_friendly" => "boolean"
 );
 
-foreach ($descs as $value => $type) {
-  echo "<rdf:Description rdf:about=\"" . $ns . "descriptorId=" . md5($value) . "\">\n";
-  echo "  <rdf:type rdf:resource=\"&bodo;Descriptor\" />\n";
-  echo "  <rdfs:label>" . $value . "</rdfs:label>\n";
-  echo "</rdf:Description>\n";
-}
+#foreach ($descs as $value => $type) {
+#  echo "<rdf:Description rdf:about=\"" . $ns . "descriptorId=" . md5($value) . "\">\n";
+#  echo "  <rdf:type rdf:resource=\"&bodo;Descriptor\" />\n";
+#  echo "  <rdfs:label>" . $value . "</rdfs:label>\n";
+#  echo "</rdf:Description>\n";
+#}
 
 $allIDs = mysql_query("SELECT * FROM compounds, compound_properties WHERE compounds.molregno = compound_properties.molregno" . $limit);
 
 $num = mysql_numrows($allIDs);
 
 while ($row = mysql_fetch_assoc($allIDs)) {
-  echo "<rdf:Description rdf:about=\"" . $ns . "moleculeId=" . $row['molregno'] . "\">\n";
-  echo "  <rdf:type rdf:resource=\"" . $ns . "Compound\" />\n";
+  echo "mol:" . $row['chebi_id'] . " a :Compound ;\n";
   if ($row['inchi']) {
-    echo "  <owl:sameAs rdf:resource=\"http://rdf.openmolecules.net/?" . $row['inchi'] . "\" />\n";
-    echo "  <chem:inchi>" . $row['inchi'] . "</chem:inchi>\n";
+    echo " = <http://rdf.openmolecules.net/?" . $row['inchi'] . "> ;\n";
+    echo " chem:inchi \"" . $row['inchi'] . "\" ;\n";
   }
-  if ($row['chebi_id'])
-    echo "  <owl:sameAs rdf:resource=\"http://bio2rdf.org/ebi:" . $row['chebi_id'] . "\" />\n";
   if ($row['inchi_key'])
-    echo "  <chem:inchikey>" . $row['inchi_key'] . "</chem:inchikey>\n";
+    echo " chem:inchikey \"" . $row['inchi_key'] . "\" ;\n";
   if ($row['canonical_smiles'])
-    echo "  <chem:smiles>" . $row['canonical_smiles'] . "</chem:smiles>\n";
-  foreach ($descs as $value => $type) {
+    echo " chem:smiles \"" . $row['canonical_smiles'] . "\" ;\n";
+
+  #foreach ($descs as $value => $type) {
     #if ($row[$row[$value]]) {
     if (false) {
       echo "  <bodo:hasDescriptorValue>\n";
@@ -83,16 +78,15 @@ while ($row = mysql_fetch_assoc($allIDs)) {
       echo "    </bodo:DescriptorValue>\n";
       echo "  </bodo:hasDescriptorValue>\n";
     }
-  }
+  #}
 
   $names = mysql_query("SELECT DISTINCT * FROM compound_synonyms WHERE molregno = " . $row['molregno']);
   while ($name = mysql_fetch_assoc($names)) {
     if ($name['synonyms'])
-      echo "  <dc:title>" . $name['synonyms'] . "</dc:title>\n";
+      echo " dc:title \"" . str_replace("\"", "\\\"", $name['synonyms']) . "\" ;\n";
   }
-  echo "</rdf:Description>\n";
+
+  echo " = <http://bio2rdf.org/ebi:" . $row['chebi_id'] . "> .\n";
 }
 
 ?>
-
-</rdf:RDF>
--- docs.php
+++ docs.php
@@ -1,28 +1,28 @@
-<?php header('Content-type: application/rdf+xml');
-print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"); 
-?>
-<!DOCTYPE rdf:RDF [
-  <!ENTITY gt "&#62;">
-  <!ENTITY lt "&#60;">
-  <!ENTITY ch "http://pele.farmbio.uu.se/chembl/?">
-  <!ENTITY bodo "http://www.blueobelisk.org/ontologies/chemoinformatics-algorithms/#">
-]>
-<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-         xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
-         xmlns:nmr="http://www.nmrshiftdb.org/onto#"
-         xmlns:chembl="&ch;"
-         xmlns:chem="http://www.blueobelisk.org/chemistryblogs/"
-         xmlns:dc="http://purl.org/dc/elements/1.1/"
-         xmlns:foaf="http://xmlns.com/foaf/0.1/"
-         xmlns:bodo="&bodo;"
-         xmlns:owl="http://www.w3.org/2002/07/owl#"
-         xmlns:bibo="http://purl.org/ontology/bibo/">
+<?php header('Content-type: application/rdf+xml'); ?>
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix owl: <http://www.w3.org/2002/07/owl#> .
+
+@prefix dc: <http://purl.org/dc/elements/1.1/> .
+@prefix bibo: <http://purl.org/ontology/bibo/> .
+@prefix foaf: <http://xmlns.com/foaf/0.1/> .
+
+@prefix bodo: <http://www.blueobelisk.org/ontologies/chemoinformatics-algorithms/#> .
+@prefix chem: <http://www.blueobelisk.org/chemistryblogs/> .
+@prefix nmr: <http://www.nmrshiftdb.org/onto#> .
+
+@prefix : <http://pele.farmbio.uu.se/chembl/onto/#> .
+@prefix act: <http://rdf.farmbio.uu.se/chembl/activitiy/> .
+@prefix res: <http://rdf.farmbio.uu.se/chembl/resource/> .
+@prefix mol: <http://rdf.farmbio.uu.se/chembl/molecule/> .
+@prefix ass: <http://rdf.farmbio.uu.se/chembl/assay/> .
+@prefix jrn: <http://rdf.farmbio.uu.se/chembl/assay/> .
+
+@prefix bpm: <http://bio2rdf.org/pubmed:> .
 
 <?php 
 
 include 'vars.php';
-
-$ns = "&ch;";
 
 #$limit = " LIMIT 5";
 $limit = "";
@@ -38,10 +38,8 @@ $allIDs = mysql_query("SELECT DISTINCT journal FROM docs" . $limit);
 $num = mysql_numrows($allIDs);
 
 while ($row = mysql_fetch_assoc($allIDs)) {
-  echo "<rdf:Description rdf:about=\"" . $ns . "journalId=" . md5($row['journal']) . "\">\n";
-  echo "  <rdf:type rdf:resource=\"http://purl.org/ontology/bibo/Journal\" />\n";
-  echo "  <dc:title>" . $row['journal'] . "</dc:title>\n";
-  echo "</rdf:Description>\n";
+  echo "jrn:" . md5($row['journal']) . " a bibo:Journal ;\n";
+  echo " dc:title \"" . $row['journal'] . "\" .\n";
 }
 
 $allIDs = mysql_query("SELECT DISTINCT * FROM docs" . $limit);
@@ -49,19 +47,19 @@ $allIDs = mysql_query("SELECT DISTINCT * FROM docs" . $limit);
 $num = mysql_numrows($allIDs);
 
 while ($row = mysql_fetch_assoc($allIDs)) {
-  echo "<rdf:Description rdf:about=\"" . $ns . "documentId=" . $row['doc_id'] . "\">\n";
-  echo "  <rdf:type rdf:resource=\"http://purl.org/ontology/bibo/Article\" />\n";
-  if ($row['doi']) echo "  <bibo:doi>" . $row['doi'] . "</bibo:doi>\n";
-  if ($row['pubmed_id']) echo "  <bibo:pmid>" . $row['pubmed_id'] . "</bibo:pmid>\n";
-  echo "  <dc:date>" . $row['year'] . "</dc:date>\n";
-  echo "  <bibo:volume>" . $row['volume'] . "</bibo:volume>\n";
-  echo "  <bibo:issue>" . $row['issue'] . "</bibo:issue>\n";
-  echo "  <bibo:pageStart>" . $row['first_page'] . "</bibo:pageStart>\n";
-  echo "  <bibo:pageEnd>" . $row['last_page'] . "</bibo:pageEnd>\n";
-  echo "  <dc:isPartOf rdf:resource=\"" . $ns . "journalId=" . md5($row['journal']) . "\" />\n";
-  echo "</rdf:Description>\n";
+  echo "res:" . $row['doc_id'] . " a bibo:Article ;\n";
+  if ($row['doi'])
+    echo " bibo:doi \"" . $row['doi'] . "\" ;\n";
+  if ($row['pubmed_id']) {
+    echo " bibo:pmid bpm:" . $row['pubmed_id'] . " ;\n";
+    echo " = bpm:" . $row['pubmed_id'] . " ;\n";
+  }
+  echo " dc:date \"" . $row['year'] . "\" ;\n";
+  echo " bibo:volume \"" . $row['volume'] . "\" ;\n";
+  echo " bibo:issue \"" . $row['issue'] . "\" ;\n";
+  echo " bibo:pageStart \"" . $row['first_page'] . "\" ;\n";
+  echo " bibo:pageEnd \"" . $row['last_page'] . "\" ;\n";
+  echo " dc:isPartOf jrn:" . md5($row['journal']) . " .\n";
 }
 
 ?>
-
-</rdf:RDF>
