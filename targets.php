d83a9c9ecc9e2ab16eac09bfd4c34ea53db7a28d
Egon Willighagen <egon.willighagen@gmail.com>
2011-08-15 12:46:51 +0200

    Output NTs for the targets

--- targets.php
+++ targets.php
@@ -1,29 +1,10 @@
 <?php header('Content-type: application/rdf+xml'); ?>
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix owl: <http://www.w3.org/2002/07/owl#> .
 
-@prefix dc: <http://purl.org/dc/elements/1.1/> .
-@prefix bibo: <http://purl.org/ontology/bibo/> .
-@prefix foaf: <http://xmlns.com/foaf/0.1/> .
-
-@prefix bodo: <http://www.blueobelisk.org/ontologies/chemoinformatics-algorithms/#> .
-@prefix chem: <http://www.blueobelisk.org/chemistryblogs/> .
-@prefix nmr: <http://www.nmrshiftdb.org/onto#> .
-
-<?php 
+<?php
 
 include 'vars.php';
-
-echo "@prefix : <http://rdf.farmbio.uu.se/chembl/onto/#> .\n";
-echo "@prefix act: <" . $rooturi . "activity/> .\n";
-echo "@prefix res: <" . $rooturi . "resource/> .\n";
-echo "@prefix mol: <" . $rooturi . "molecule/> .\n";
-echo "@prefix ass: <" . $rooturi . "assay/> .\n";
-echo "@prefix jrn: <" . $rooturi . "journal/> .\n";
-echo "@prefix trg: <" . $rooturi . "target/> .\n";
-echo "@prefix tgt: <" . $rooturi . "targetType/> .\n";
-echo "\n";
+include 'namespaces.php';
+include 'functions.php';
 
 mysql_connect("localhost", $user, $pwd) or die(mysql_error());
 # echo "<!-- Connection to the server was successful! -->\n";
@@ -36,50 +17,51 @@ $allIDs = mysql_query("SELECT DISTINCT * FROM target_dictionary" . $limit);
 $num = mysql_numrows($allIDs);
 
 while ($row = mysql_fetch_assoc($allIDs)) {
-  echo "trg:t" . $row['tid'] . " a :Target ;\n";
-  echo " :hasTargetType tgt:" . $row['target_type'] . " ;\n";
+  $target = $TRG . "t" . $row['tid'];
+  echo triple( $target, $RDF . "type", $ONTO . "Target" );
+  echo triple( $target, $ONTO . "hasTargetType", $TGT . $row['target_type'] );
   if ($row['organism'])
-    echo " :organism \"" . $row['organism'] . "\" ;\n";
+    echo dataTriple( $target, $ONTO . "organism", $row['organism'] );
   if ($row['description'])
-    echo " :hasDescription \"" . str_replace("\"", "\\\"", $row['description']) . "\" ;\n"; 
+    echo dataTriple( $target, $ONTO . "hasDescription",  str_replace("\"", "\\\"", $row['description']) ); 
   if ($row['synonyms']) {
     $synonyms = preg_split("/[;]+/", $row['synonyms']);
     foreach ($synonyms as $i => $synonym) {
-      echo " rdfs:label \"" . str_replace("\"", "\\\"", trim($synonym)) . "\" ;\n";
+      echo dataTriple( $target, $RDFS . "label", str_replace("\"", "\\\"", trim($synonym)) );
     }
   }
   if ($row['keywords']) {
     $keywords = preg_split("/[;]+/", $row['keywords']);
     foreach ($keywords as $i => $keyword) {
-      echo " :hasKeyword \"" . str_replace("\"", "\\\"", trim($keyword)) . "\" ;\n";
+      echo dataTriple( $target, $ONTO . "hasKeyword", str_replace("\"", "\\\"", trim($keyword)) );
     }
   }
   if ($row['protein_sequence'])
-    echo " :sequence \"" . $row['protein_sequence'] . "\" ;\n";
+    echo dataTriple( $target, $ONTO . "sequence", $row['protein_sequence'] );
   if ($row['ec_number']) {
-    echo " dc:identifier \"" . $row['ec_number'] . "\" ;\n";
-    echo " owl:sameAs <http://bio2rdf.org/ec:" . $row['ec_number'] . "> ;\n";
+    echo dataTriple( $target, $DC . "identifier", $row['ec_number'] );
+    echo triple( $target, $OWL . "sameAs", "http://bio2rdf.org/ec:" . $row['ec_number'] );
   }
   if ($row['protein_accession'])
-    echo " dc:identifier \"uniprot:" . $row['protein_accession'] . "\" ;\n";
-    echo " owl:sameAs <http://bio2rdf.org/uniprot:" . $row['protein_accession'] . "> ;\n";
+    echo dataTriple( $target, $DC . "identifier",  "uniprot:" . $row['protein_accession'] );
+    echo triple( $target, $OWL . "sameAs", "http://bio2rdf.org/uniprot:" . $row['protein_accession'] );
   if ($row['tax_id'])
-    echo " :hasTaxonomy <http://bio2rdf.org/taxonomy:" . $row['tax_id'] . "> ;\n";
+    echo triple( $target, $ONTO . "hasTaxonomy", "http://bio2rdf.org/taxonomy:" . $row['tax_id'] );
 
   # classifications
   $class = mysql_query("SELECT DISTINCT * FROM target_class WHERE tid = \"" . $row['tid'] . "\"");
   if ($classRow = mysql_fetch_assoc($class)) {
-    if ($classRow['l1']) echo " :classL1 \"" . str_replace("\"", "\\\"", $classRow['l1']) . "\" ;\n";
-    if ($classRow['l2']) echo " :classL2 \"" . str_replace("\"", "\\\"", $classRow['l2']) . "\" ;\n";
-    if ($classRow['l3']) echo " :classL3 \"" . str_replace("\"", "\\\"", $classRow['l3']) . "\" ;\n";
-    if ($classRow['l4']) echo " :classL4 \"" . str_replace("\"", "\\\"", $classRow['l4']) . "\" ;\n";
-    if ($classRow['l5']) echo " :classL5 \"" . str_replace("\"", "\\\"", $classRow['l5']) . "\" ;\n";
-    if ($classRow['l6']) echo " :classL6 \"" . str_replace("\"", "\\\"", $classRow['l6']) . "\" ;\n";
-    if ($classRow['l7']) echo " :classL7 \"" . str_replace("\"", "\\\"", $classRow['l7']) . "\" ;\n";
-    if ($classRow['l8']) echo " :classL8 \"" . str_replace("\"", "\\\"", $classRow['l8']) . "\" ;\n";
+    if ($classRow['l1']) echo dataTriple( $target, $ONTO . "classL1", str_replace("\"", "\\\"", $classRow['l1']) );
+    if ($classRow['l2']) echo dataTriple( $target, $ONTO . "classL2", str_replace("\"", "\\\"", $classRow['l2']) );
+    if ($classRow['l3']) echo dataTriple( $target, $ONTO . "classL3", str_replace("\"", "\\\"", $classRow['l3']) );
+    if ($classRow['l4']) echo dataTriple( $target, $ONTO . "classL4", str_replace("\"", "\\\"", $classRow['l4']) );
+    if ($classRow['l5']) echo dataTriple( $target, $ONTO . "classL5", str_replace("\"", "\\\"", $classRow['l5']) );
+    if ($classRow['l6']) echo dataTriple( $target, $ONTO . "classL6", str_replace("\"", "\\\"", $classRow['l6']) );
+    if ($classRow['l7']) echo dataTriple( $target, $ONTO . "classL7", str_replace("\"", "\\\"", $classRow['l7']) );
+    if ($classRow['l8']) echo dataTriple( $target, $ONTO . "classL8", str_replace("\"", "\\\"", $classRow['l8']) );
   }
 
-  echo " dc:title \"" . $row['pref_name'] . "\" .\n";
+  echo dataTriple( $target, $DC . "title", $row['pref_name'] );
 }
 
 ?>
