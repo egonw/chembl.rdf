5ec601b0d8fe52fbfce630dcce4f5e12c02c7ca5
Egon Willighagen <egon.willighagen@gmail.com>
2011-08-15 09:05:32 +0200

    Reworked code to spit n-triples

--- docs.php
+++ docs.php
@@ -1,27 +1,23 @@
 <?php header('Content-type: application/rdf+xml'); ?>
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix owl: <http://www.w3.org/2002/07/owl#> .
-
-@prefix dc: <http://purl.org/dc/elements/1.1/> .
-@prefix bibo: <http://purl.org/ontology/bibo/> .
-@prefix foaf: <http://xmlns.com/foaf/0.1/> .
-
-@prefix bodo: <http://www.blueobelisk.org/ontologies/chemoinformatics-algorithms/#> .
-@prefix chem: <http://www.blueobelisk.org/chemistryblogs/> .
-@prefix nmr: <http://www.nmrshiftdb.org/onto#> .
 
 <?php
 
 include 'vars.php';
+include 'namespaces.php';
 
-echo "@prefix : <http://rdf.farmbio.uu.se/chembl/onto/#> .\n";
-echo "@prefix act: <" . $rooturi . "activity/> .\n";
-echo "@prefix res: <" . $rooturi . "resource/> .\n";
-echo "@prefix mol: <" . $rooturi . "molecule/> .\n";
-echo "@prefix ass: <" . $rooturi . "assay/> .\n";
-echo "@prefix jrn: <" . $rooturi . "journal/> .\n";
-echo "\n";
+function triple($subject, $predicate, $object) {
+  return "<" . $subject . "> <" . $predicate . "> <" . $object . "> .\n";
+}
+function dataTriple($subject, $predicate, $object) {
+  return "<" . $subject . "> <" . $predicate . "> \"" . $object . "\" .\n";
+}
+
+$ONTO = "http://rdf.farmbio.uu.se/chembl/onto/#";
+$ACT = $rooturi . "activity/";
+$RES = $rooturi . "resource/";
+$MOL = $rooturi . "molecule/";
+$ASS = $rooturi . "assay/";
+$JRN = $rooturi . "journal/";
 
 mysql_connect("localhost", $user, $pwd) or die(mysql_error());
 # echo "<!-- Connection to the server was successful! -->\n";
@@ -34,29 +30,33 @@ $allIDs = mysql_query("SELECT DISTINCT journal FROM docs" . $limit);
 $num = mysql_numrows($allIDs);
 
 while ($row = mysql_fetch_assoc($allIDs)) {
-  echo "jrn:j" . md5($row['journal']) . " a bibo:Journal ;\n";
-  echo " dc:title \"" . $row['journal'] . "\" .\n";
+  if (strlen($row['journal']) > 0) {
+    echo triple($JRN . "j" . md5($row['journal']), $RDF . "type", $BIBO . "Journal");
+    echo datatriple($JRN . "j" . md5($row['journal']), $DC . "title", $row['journal']);
+  }
 }
+echo "\n";
 
 $allIDs = mysql_query("SELECT DISTINCT * FROM docs WHERE doc_id > 0" . $limit);
 
 $num = mysql_numrows($allIDs);
 
 while ($row = mysql_fetch_assoc($allIDs)) {
-  echo "res:r" . $row['doc_id'] . " a bibo:Article ;\n";
+  $resource = $RES . "r" . $row['doc_id'];
+  echo triple( $resource, $RDF . "type", $BIBO . ":Article" );
   if ($row['doi']) {
-    echo " bibo:doi \"" . $row['doi'] . "\" ;\n";
-    echo " owl:sameAs <http://dx.doi.org/" . $row['doi'] . "> ;\n";
+    echo dataTriple( $resource, $BIBO . "doi", $row['doi'] );
+    echo triple( $resource, $OWL . "sameAs",  "http://dx.doi.org/" . $row['doi'] );
   }
   if ($row['pubmed_id']) {
-    echo " bibo:pmid <http://bio2rdf.org/pubmed:" . $row['pubmed_id'] . "> ;\n";
+    echo triple( $resource, $BIBO . "pmid", "http://bio2rdf.org/pubmed:" . $row['pubmed_id'] );
   }
-  echo " dc:date \"" . $row['year'] . "\" ;\n";
-  echo " bibo:volume \"" . $row['volume'] . "\" ;\n";
-  echo " bibo:issue \"" . $row['issue'] . "\" ;\n";
-  echo " bibo:pageStart \"" . $row['first_page'] . "\" ;\n";
-  echo " bibo:pageEnd \"" . $row['last_page'] . "\" ;\n";
-  echo " dc:isPartOf jrn:j" . md5($row['journal']) . " .\n";
+  echo dataTriple( $resource, $DC . "date", $row['year'] );
+  echo dataTriple( $resource, $BIBO . "volume", $row['volume'] );
+  echo dataTriple( $resource, $BIBO . "issue", $row['issue'] );
+  echo dataTriple( $resource, $BIBO . "pageStart", $row['first_page'] );
+  echo dataTriple( $resource, $BIBO . "pageEnd", $row['last_page'] );
+  echo triple( $resource, $DC . "isPartOf", $JRN . "j" . md5($row['journal']) );
 }
 
 ?>
---
+++ namespaces.php
@@ -0,0 +1,15 @@
+<?php
+
+$RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+$RDFS = "http://www.w3.org/2000/01/rdf-schema#";
+$OWL = "http://www.w3.org/2002/07/owl#";
+
+$DC = "http://purl.org/dc/elements/1.1/";
+$BIBO = "http://purl.org/ontology/bibo/";
+$FOAF = "http://xmlns.com/foaf/0.1/";
+
+$BODO = "http://www.blueobelisk.org/ontologies/chemoinformatics-algorithms/#";
+$CHEM = "http://www.blueobelisk.org/chemistryblogs/";
+$NMR = "http://www.nmrshiftdb.org/onto#";
+
+?>
