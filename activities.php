33b0843aec74bae1d5ba66a8dd4dcf70ed0b59ce
Egon Willighagen <egon.willighagen@gmail.com>
2012-01-17 18:47:06 +0100

    Fixed picking the right identifier for the forMolecule triple

--- activities.php
+++ activities.php
@@ -1,6 +1,4 @@
-<?php header('Content-type: text/n3'); ?>
-
-<?php 
+<?php header('Content-type: text/n3');
 
 include 'vars.php';
 include 'namespaces.php';
@@ -10,7 +8,7 @@ mysql_connect("localhost", $user, $pwd) or die(mysql_error());
 echo "# Connection to the server was successful!\n";
 
 mysql_select_db($db) or die(mysql_error());
-echo "# Database " . $db . " was selected!\n";
+echo "# Database " . $db . " was selected!\n\n";
 
 $allIDs = mysql_query("SELECT DISTINCT * FROM activities" . $limit);
 
@@ -19,10 +17,7 @@ while ($row = mysql_fetch_assoc($allIDs)) {
   echo triple( $activity, $RDF . "type",  $ONTO . "Activity" );
   echo triple( $activity, $CITO . "citesAsDataSource", $RES . "r" . $row['doc_id'] );
   echo triple( $activity, $ONTO . "onAssay", $ASS . "a" . $row['assay_id'] );
-  $chebi = mysql_query("SELECT DISTINCT * FROM molecule_dictionary WHERE molregno = \"" . $row['molregno'] . "\"");
-  if ($chebiRow = mysql_fetch_assoc($chebi)) {
-    echo triple( $activity, $ONTO . "forMolecule", $MOL . "m" . $chebiRow['chebi_id'] );
-  }
+  echo triple( $activity, $ONTO . "forMolecule", $MOL . "m" . $row['molregno'] );
   if ($row['relation']) {
     echo dataTriple( $activity, $ONTO . "relation",  $row['relation'] );
   }
