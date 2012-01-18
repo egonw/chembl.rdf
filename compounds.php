a4051954d26733439fde9d44b3f968c88f7b3d56
Egon Willighagen <egon.willighagen@gmail.com>
2012-01-18 08:13:52 +0100

    Removed the outdated descriptor stuff; will be in CHEMINF later

--- compounds.php
+++ compounds.php
@@ -12,24 +12,6 @@ mysql_connect("localhost", $user, $pwd) or die(mysql_error());
 mysql_select_db($db) or die(mysql_error());
 # echo "<!-- Database was selected! -->\n";
 
-$descs = array(
-  "alogp" => "double",
-  "hba" => "nonNegativeInteger",
-  "hbd" => "nonNegativeInteger",
-  "psa" => "double",
-  "rtb" => "nonNegativeInteger",
-  "ro3_pass" => "boolean",
-  "num_ro5_violations" => "nonNegativeInteger",
-  "med_chem_friendly" => "boolean"
-);
-
-#foreach ($descs as $value => $type) {
-#  echo "<rdf:Description rdf:about=\"" . $ns . "descriptorId=" . md5($value) . "\">\n";
-#  echo "  <rdf:type rdf:resource=\"&bodo;Descriptor\" />\n";
-#  echo "  <rdfs:label>" . $value . "</rdfs:label>\n";
-#  echo "</rdf:Description>\n";
-#}
-
 $allIDs = mysql_query(
   "SELECT DISTINCT compound_records.molregno FROM compound_records, compound_properties " .
   "WHERE compound_records.molregno = compound_properties.molregno " . $limit
@@ -90,23 +72,6 @@ while ($row = mysql_fetch_assoc($allIDs)) {
         echo dataTriple($molsmiles, $CHEMINF . "SIO_000300", $struct['standard_inchi_key']);
       }
     }
-
-    #foreach ($descs as $value => $type) {
-      #if ($row[$row[$value]]) {
-      if (false) {
-        echo "  <bodo:hasDescriptorValue>\n";
-        echo "    <bodo:DescriptorValue>\n";
-        echo "      <bodo:hasPart>\n";
-        echo "        <bodo:DescriptorValuePoint>\n";
-        echo "          <bodo:hasValue rdf:datatype=\"http://www.w3.org/2001/XMLSchema#" .
-            $type . "\">" . $row[$value] . "</bodo:hasValue>\n";
-        echo "          <bodo:valuePointFor rdf:resource=\"" . $ns . "descriptorId=" . md5($value) . "\" />\n";
-        echo "        </bodo:DescriptorValuePoint>\n";
-        echo "      </bodo:hasPart>\n";
-        echo "    </bodo:DescriptorValue>\n";
-        echo "  </bodo:hasDescriptorValue>\n";
-      }
-    #}
 
     $names = mysql_query("SELECT DISTINCT * FROM molecule_synonyms WHERE molregno = " . $row['molregno']);
     while ($name = mysql_fetch_assoc($names)) {
