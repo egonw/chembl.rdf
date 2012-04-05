dc40bc21a5c6b1e4ffbffc841052db1731d7bae5
Egon Willighagen <egon.willighagen@gmail.com>
2012-04-05 12:19:55 +0200

    Added SKOS

--- namespaces.php
+++ namespaces.php
@@ -4,6 +4,7 @@ $RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 $RDFS = "http://www.w3.org/2000/01/rdf-schema#";
 $OWL = "http://www.w3.org/2002/07/owl#";
 $XSD = "http://www.w3.org/2001/XMLSchema#";
+$SKOS = "http://www.w3.org/2004/02/skos/core#";
 
 $DC = "http://purl.org/dc/elements/1.1/";
 $BIBO = "http://purl.org/ontology/bibo/";
