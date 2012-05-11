024e1786eb92f9e668a34950710ad09c4c70d6d9
Egon Willighagen <egon.willighagen@gmail.com>
2012-05-11 15:22:09 +0200

    Fixed the UNIPROT namespace

--- namespaces.php
+++ namespaces.php
@@ -23,7 +23,7 @@ $CHEBI = "http://purl.org/obo/owl/CHEBI#";
 $PRO = $OBO;
 $VO = $OBO;
 $CL = $OBO;
-$UNIPROT = "http://www.uniprot.org/uniprot/";
+$UNIPROT = "http://purl.uniprot.org/uniprot/";
 
 $ONTO = "http://rdf.farmbio.uu.se/chembl/onto/#";
 $ACT = $rooturi . "activity/";
