d70388bd4619189f22885cd3ee3ecfd9e5866cd6
Egon Willighagen <egon.willighagen@gmail.com>
2012-07-10 19:10:52 +0200

    Updated for the to.php changes: key is now a code, and the label is a field; Also now outputs an example and a scope.

--- targetOntology2.php
+++ targetOntology2.php
@@ -8,13 +8,17 @@ include 'functions.php';
 include 'to.php';
 
 # classifications
-foreach ($array as $desc => $array2) {
+foreach ($array as $code => $array2) {
     $higher = $array2["higher"];
     $uri = $array2["uri"];
+    $desc = $array2["label"];
+    $example = $array2["classification"];
     echo triple($uri, $RDF . "type", $SKOS . "Concept");
     echo triple($uri, $RDFS . "subClassOf", $higher);
     echo triple($higher, $SKOS . "narrower", $uri);
     echo data_triple($uri, $SKOS . "prefLabel", $desc);
+    echo data_triple($uri, $SKOS . "scopeNote", $code);
+    echo data_triple($uri, $SKOS . "example", $example);
 }
 
 ?>
