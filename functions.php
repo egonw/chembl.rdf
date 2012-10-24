a23d1f544224e94a20b2a332c2d852e1e44ebfaf
Egon Willighagen <egon.willighagen@gmail.com>
2012-10-24 20:12:06 +0200

    Type all literals as xsd:string by default

--- functions.php
+++ functions.php
@@ -4,7 +4,8 @@ function triple($subject, $predicate, $object) {
   return "<" . $subject . "> <" . $predicate . "> <" . $object . "> .\n";
 }
 function data_triple($subject, $predicate, $object) {
-  return "<" . $subject . "> <" . $predicate . "> \"" . $object . "\" .\n";
+  $XSD = "http://www.w3.org/2001/XMLSchema#";
+  return typeddata_triple($subject, $predicate, $object, $XSD . "string");
 }
 function typeddata_triple($subject, $predicate, $object, $type) {
   return "<" . $subject . "> <" . $predicate . "> \"" . $object . "\"^^<" . $type . "> .\n";
