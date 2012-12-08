823a99cccd008b6faef055239bdd218bf6a6a93b
Egon Willighagen <egon.willighagen@gmail.com>
2012-12-08 17:37:14 +0100

    Complete Open PHACTS compatible data set VoID

--- createVoid.php
+++ createVoid.php
@@ -5,8 +5,10 @@
   $subversion = $ini["subversion"];
   $importedOn = $ini["importedOn"];
   $importedBy = $ini["importedBy"];
+  $datadump = $ini["datadump"];
 ?>
 @prefix void: <http://rdfs.org/ns/void#> .
+@prefix voag: <http://voag.linkedmodel.org/voag#> .
 @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
 @prefix owl: <http://www.w3.org/2002/07/owl#> .
@@ -19,22 +21,30 @@
 
 <?php print("@prefix : <" . $rooturi . "void.ttl#> .\n"); ?>
 
+:ChEMBL foaf:homepage <https://www.ebi.ac.uk/chembl/> ;
+  dcterms:title "ChEMBL" .
+
 :ChEMBLRDF a void:Dataset ;
   foaf:homepage <https://github.com/egonw/chembl.rdf> ;
   dcterms:title "ChEMBL-RDF" ;
   dcterms:description "RDF data extracted from ChEMBL, a CC-BY-SA database developed at the EBI by J. Overington et al." ;
+  pav:derivedFrom :ChEMBL ;
+  dcterms:subject <http://live.dbpedia.org/resource/Pharmacology> ;
 <?php
   $current_date = gmDate("Y-m-d\TH:i:s");
-  print("  pav:authoredBy <" . $importedBy . "> ;\n");
-  print("  pav:authoredOn \"" . $current_date . "\"^^xsd:dateTime ;\n");
+  print("  pav:createdBy <" . $importedBy . "> ;\n");
+  print("  pav:createdOn \"" . $current_date . "\"^^xsd:dateTime ;\n");
   print("  pav:createdWith <https://raw.github.com/openphacts/chembl.rdf/master/createVoid.php> ;\n");
   // print("  dcterms:modified \"" . "\" ;\n");
   print("  void:uriSpace \"" . $rooturi . "\" ;\n");
   print("  pav:version \"" . $version . "_" . $subversion . "\" ; \n");
   print("  pav:importedOn \"" . $importedOn . "\"^^xsd:dateTime ; \n");
-  print("  pav:importedBy \"" . $importedBy . "\" ; \n");
-  print("  pav:importedFrom \"ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/releases/chembl_" . $version . "/\" ; \n");
+  print("  pav:importedBy <" . $importedBy . "> ; \n");
+  print("  pav:importedFrom <ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/releases/chembl_" . $version . "/> ; \n");
+  print("  void:dataDump <" . $datadump . "> ;\n");
+  print("  void:exampleResource <" . $rooturi .  "chemblid/CHEMBL146554> ;\n");
 ?>
+  voag:frequencyOfChange voag:UncertainFrequency ;
   void:vocabulary <http://xmlns.com/foaf/0.1/> ;
   void:vocabulary <http://purl.org/dc/terms/>;
   void:vocabulary <http://purl.org/spar/cito/> ;
@@ -42,6 +52,11 @@
   dcterms:license <http://creativecommons.org/licenses/by-sa/3.0/> .
 
 <> a void:DatasetDescription;
-  dcterms:title "A VoID Description of the ChEMBL-RDF dataset" ;
-  dcterms:creator <http://egonw.github.com/#me> ;
+  dcterms:title "ChEMBL-RDF dataset VoID description" ;
+  dcterms:description "A VoID description of the ChEMBL-RDF dataset." ;
+<?php
+  print("  pav:createdOn \"" . $current_date . "\"^^xsd:dateTime ;\n");
+  print("  pav:lastUpdateOn \"" . $current_date . "\"^^xsd:dateTime ;\n");
+  print("  pav:createdBy <" . $importedBy . "> ;\n");
+?>
   foaf:primaryTopic :ChEMBLRDF .
